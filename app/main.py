9adc1b06922e6b25134ee1ce5cc532bd0a4baf8b
Maria Maqsood <mariamaqsoodva@gmail.com>
2025-07-21 15:12:34 +0500

    Remove retrain path

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 from app.model_utils import load_models
 import joblib
-import os
 
 app = FastAPI()
 
@@ -39,25 +38,3 @@ def predict(data: SensorData):
         "maintenance_prediction": class_label,
         "anomaly_status": anomaly_status
     }
-
-@app.post("/retrain")
-def retrain():
-    csv_path = "machine_dataset.csv"
-    if not os.path.exists(csv_path):
-        raise HTTPException(status_code=404, detail="machine_dataset.csv not found")
-
-    df = pd.read_csv(csv_path)
-    X = df[feature_columns]
-    y = df["maintenance"]
-
-    label_encoder.fit(y)
-    y_encoded = label_encoder.transform(y)
-
-    clf.fit(X, y_encoded)
-    anomaly_model.fit(X)
-
-    joblib.dump(clf, "./models/classifier.pkl")
-    joblib.dump(anomaly_model, "./models/anomaly_detector.pkl")
-    joblib.dump(label_encoder, "./models/encoder.pkl")
-
-    return {"status": "Models retrained successfully"}
